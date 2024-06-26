5c37e588b45da2395286b744cbc28f7bafe079ad
truongthuanr <truongthuan.r@gmail.com>
2024-06-26 23:04:14 +0700

    add + remove

--- 02_JS/F01_QLHV/course.js
+++ 02_JS/F01_QLHV/course.js
@@ -1,19 +1,29 @@
 class course {
     name;
-    listStudent;
+    listStudents;
 
     constructor(courseName){
         this.name = courseName;
-        this.listStudents = []
+        this.listStudents = [];
 
+    }
+
+    readLocalStorage(local){
+        this.listStudents = this.listStudents.concat(local.listStudents)
     }
 
     addStudent(newStudent){
-        this.listStudents.push(newStudent)
+        console.log("Run course.addStuden")
+        let temp = JSON.stringify(newStudent)
+        temp = JSON.parse(temp)
+        this.listStudents.push(temp)
 
+        showAll()
     }
 
-    removeStudent(){
+    removeStudent(i){
+        console.log(`Run remove student index ${i}`)
+        this.listStudents.splice(i)
 
     }
 
--- 02_JS/F01_QLHV/main.js
+++ 02_JS/F01_QLHV/main.js
@@ -1,7 +1,3 @@
-// myCourse = new course("Javascript Course")
-
-
-
 // let stu1 = new Student(1, "Nguyen Truong Thuan", "01/01/2000", "Nam", "nguyentruongthuan@gmail.com")
 // let stu2 = new Student(2, "Ho Ngoc Ha", "02/02/2002", "Nữ", "ngocha@gmail.com")
 // let stu3 = new Student(3, "Tran Van Thanh", "03/03/2001", "Nam", "vanthanhtran@gmail.com")
@@ -12,58 +8,107 @@
 // localStorage.setItem('myCourse',JSON.stringify(myCourse))
 // console.log(localStorage.getItem('myCourse'))
 
-myCourse = JSON.parse(localStorage.getItem("myCourse"))
+let local = JSON.parse(localStorage.getItem("myCourse"))
+console.log(local.listStudents)
+
+let myCourse = new course("Javascript Course")
+myCourse.readLocalStorage(local)
+console.log(myCourse.listStudents)
+
+
+// coursetemp = JSON.stringify(myCourse.listStudents)
+// coursetemp = JSON.parse(coursetemp)
+// console.log(coursetemp)
+// console.log(myCourse.listStudents)
+// let temp = local.listStudents || []
+// temp = temp.concat(coursetemp)
+// console.log(temp)
+
+// console.log(`Local storage ${local.}`)
+// local.addStudent()
 
 showAll()
 
 console.log("asdfasfasdf")
 
 // avoid resubmitsion form
-if ( window.history.replaceState ) {
-    window.history.replaceState( null, null, window.location.href );
-  }
+if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
+}
 
 
 let newstudent_form = document.getElementById('addnewstudent-form')
-newstudent_form.addEventListener("submit",submitNewStudent)
+newstudent_form.addEventListener("submit", submitNewStudent)
 
 function addNewStudent() {
     document.getElementById('addnewstudent-form').style.display = 'block'
 }
 
-function showDetail() {
+function showDetail(i) {
+    console.log('Run showDetail()')
+    let studentdetail = document.getElementById('student-detail')
+    console.log(myCourse.listStudents[i])
+    studentdetail.innerHTML = myCourse.listStudents[i].name
 
 }
 
-function submitNewStudent(event){
+function submitNewStudent(event) {
     console.log('Submit new student form')
 
 
 
     let newstudent = newstudent_form.elements
+    let valid = false;
 
 
     // Form validation
-    console.log(`New student ID ${newstudent_form.elements.namedItem('student-id').value}`)
-    console.log(`New student Name ${newstudent_form.elements.namedItem('student-name').value}`)
-    console.log(`New student Birthday ${newstudent_form.elements.namedItem('student-birthday').value}`)
+    // console.log(`New student ID ${newstudent_form.elements.namedItem('student-id').value}`)
+    // console.log(`New student Name ${newstudent_form.elements.namedItem('student-name').value}`)
+    // console.log(`New student Birthday ${newstudent_form.elements.namedItem('student-birthday').value}`)
+    let studentid = newstudent_form.elements.namedItem('student-id').value;
+    let studentname = newstudent_form.elements.namedItem('student-name').value;
+    let studentbirthday = newstudent_form.elements.namedItem('student-birthday').value
     let gender = document.querySelector('input[type=radio][name=student-gender]:checked')
+    let studentemail = newstudent_form.elements.namedItem('student-email').value
+
+    let studentcourse = newstudent_form.elements.namedItem('student-course').value
+
+    let studentgender;
+
     console.log(`Gender ${gender}`)
 
-    if (newstudent.namedItem('student-id').value==""){
+    if (newstudent.namedItem('student-id').value == "") {
         alert("Student's ID must be not-null")
     }
-    else if (newstudent.namedItem('student-name').value==""){
+    else if (newstudent.namedItem('student-name').value == "") {
         alert("Student's Name must be not-null")
 
     }
-    else if (newstudent.namedItem('student-birthday').value==""){
+    else if (newstudent.namedItem('student-birthday').value == "") {
         alert("Student's Name must be not-null")
     }
-    else if (gender===null){
+    else if (gender === null) {
         alert("Student's Gender must be not-null")
     }
-    else if()
+    else {
+        valid = true;
+    }
+
+    if (valid == true) {
+        studentgender = gender.value;
+        console.log(`Student gender ${studentgender}`)
+        let student = new Student(studentid,
+            studentname,
+            studentbirthday,
+            studentgender,
+            studentemail,
+            studentcourse)
+
+        myCourse.addStudent(student)
+
+
+
+    }
 
 
     // Add student 
@@ -94,9 +139,9 @@ function showAll() {
         <tr>
             <td>${list[i].id}</td>
             <td>${list[i].name}</td>
-            
-
-            <td><button onclick="showDetail(${i})">Chi tiết</button></td>
+            <td>${list[i].email}</td>
+            <td><button onclick="showDetail(${i})">Detail</button>
+                <button onclick="remove(${i})">Remove</button></td>
 
         </tr>
         `
@@ -105,4 +150,16 @@ function showAll() {
         //     <td><button onclick="remove(${i})">Xóa</button></td>
     }
     document.getElementById('table-student').innerHTML = str
+}
+
+
+function SaveToStorage(){
+    localStorage.setItem('myCourse',JSON.stringify(myCourse))
+    newstudent_form.reset();
+    newstudent_form.style.display = 'None'
+}
+
+function remove(i){
+    myCourse.removeStudent(i)
+    showAll()
 }
